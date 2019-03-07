ca9657aa44631d7ba5ec0eb2ef644bec3639e0ea
linrq@bsoft.com.cn <linrq@bsoft.com.cn>
2019-03-07 17:41:40 +0800

    [code] day04：搞不定的登录唉……跳过，进军day05

--- pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -3,6 +3,8 @@ package com.pinyougou.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -16,10 +18,29 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  * @author Administrator
  */
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private SellerService sellerService;
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
-        grantedAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
-        return new User(username, "123456", grantedAuths);
+        System.out.println("经过了UserDetailsServiceImpl");
+        //构建角色列表
+        List<GrantedAuthority> grantAuths = new ArrayList();
+        grantAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+        //得到商家对象
+        TbSeller seller = sellerService.findOne(username);
+        if (seller != null) {
+            if (seller.getStatus().equals("1")) {
+                return new User(username, seller.getPassword(), grantAuths);
+            } else {
+                return null;
+            }
+        } else {
+            return new User(username, "123456", grantAuths);
+        }
     }
 }
